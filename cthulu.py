3cf34c69847bd79ed27768e7081fa356511e2cf9
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2015-07-10 21:05:11 +0200

    whitespace

--- cthulu.py
+++ cthulu.py
@@ -1,6 +1,9 @@
 should_we = lambda: False # e.g. NO and
+
 destroy_world = lambda: "Call forth Cthulhu, eater of worlds" # then if
+
 if all(f() for f in (should_we, destroy_world)):
     print("are we still here?")
+
 if any(f() for f in (should_we, destroy_world)):
     print("Yes we should")
